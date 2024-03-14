cb1607b22eb41314ddeb202e9ab38a9b0439ad6c
ademi almazova <ademialmazova21@gmail.com>
2024-03-13 22:01:43 -0500

    Jenkinsfile changed

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
     region = "us-east-2"  
 }
 resource "aws_vpc" "test" {
-  cidr_block       = "10.0.0.0/16"
+  cidr_block       = "10.0.0.0/20"
   instance_tenancy = "default"
 
   tags = {
